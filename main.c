23d34226ddd9d4dc6966c7c4c18ca822c49406d0
adarmawan117 <adarmawan117@gmail.com>
2021-09-27 13:02:16 +0700

    tambah fitur init pelanggan, tampil list pelanggan

--- main.c
+++ main.c
@@ -1,4 +1,22 @@
 /**
+Versi 2
+===========
+27-September-2021
+===========
+- Tambah Peminjam/Penyewa
+- Lihat list peminjam
+- Mengembalikan buku
+
+
+DATA Peminjam
+-------------
+id
+nama
+statusAnggota => 0 untuk bukan anggota, dan 1 untuk anggota
+
+
+##################################
+
 Versi 1
 ===========
 26-September-2021
@@ -14,6 +32,7 @@ id
 nama
 pengarang
 hargaSewa perhari
+statusSewa
 
 */
 
@@ -31,6 +50,11 @@ hargaSewa perhari
 #define MAX_NAMA 40
 #define MAX_STRUCT 50
 
+
+
+
+
+
 typedef struct {
     char id[MAX_ID];
     char namaBuku[MAX_NAMA];
@@ -38,8 +62,29 @@ typedef struct {
     int statusSewa; // 0 untuk avail, 1 untuk dipinjam
 } DATA_BUKU;
 
+typedef struct {
+    char id[MAX_ID];
+    char namaPeminjam[MAX_NAMA];
+    int statusAnggota; // 0 untuk bukan anggota, dan 1 merupakan anggota
+} DATA_PEMINJAM;
+
+
+
+
+
+
 DATA_BUKU listBuku[MAX_STRUCT];
 int banyakBuku = 0;
+
+DATA_PEMINJAM listPeminjam[MAX_STRUCT];
+int banyakPeminjam = 0;
+
+
+
+
+
+
+
 
 void tampilSatuBuku(int i) {
     printf("||%-5d||%-10s||%-40s||%-15d||", (i+1), listBuku[i].id, listBuku[i].namaBuku, listBuku[i].hargaSewa);
@@ -68,6 +113,35 @@ void tampilListBuku() {
     }
     printf("========================================================================================================\n");
 }
+
+
+void tampilSatuPeminjam(int i) {
+    printf("||%-5d||%-10s||%-40s||", (i+1), listPeminjam[i].id, listPeminjam[i].namaPeminjam);
+
+    char statusAnggota[MAX_NAMA] = "Bukan Anggota";
+    if(listPeminjam[i].statusAnggota == 1) {
+        strcpy(statusAnggota, "Anggota");
+    }
+    printf("%-25s||\n", statusAnggota);
+}
+
+void tampilListPeminjam() {
+
+    if(banyakPeminjam == 0) {
+        printf("Peminjam masih kosong!\n");
+        return;
+    }
+
+    int i;
+    printf("==========================================================================================\n");
+    printf("||%-5s||%-10s||%-40s||%-25s||\n", "No", "ID", "Nama", "Status Anggota");
+    printf("==========================================================================================\n");
+    for(i = 0; i < banyakBuku; i++) {
+        tampilSatuPeminjam(i);
+    }
+    printf("==========================================================================================\n");
+}
+
 
 void pinjamBuku() {
     // cek apakah ada buku yagn bisa dipinjam
@@ -102,12 +176,24 @@ void pinjamBuku() {
     } while(confirm != 'Y' && confirm != 'T');
 }
 
-void init() {
+void initBuku() {
     strcpy(listBuku[banyakBuku].id, "BK-01");
     strcpy(listBuku[banyakBuku].namaBuku, "Belajar Pemrograman JAVA");
     listBuku[banyakBuku].hargaSewa = 30000;
     listBuku[banyakBuku].statusSewa = 0;
     banyakBuku++;
+}
+
+void initPeminjam() {
+    strcpy(listPeminjam[banyakPeminjam].id, "PMJ-01");
+    strcpy(listPeminjam[banyakPeminjam].namaPeminjam, "Hendra Maulana");
+    listPeminjam[banyakPeminjam].statusAnggota = 0;
+    banyakPeminjam++;
+}
+
+void init() {
+    initBuku();
+    initPeminjam();
 }
 
 void menuUtama() {
@@ -118,7 +204,9 @@ void menuUtama() {
         printf("MENU UTAMA\n");
         printf("1. Tampil List Buku\n");
         printf("2. Pinjam Buku\n");
-        printf("3. Keluar\n");
+        printf("3. Tampil List Peminjam\n");
+        printf("4. Tambah Peminjam\n");
+        printf("5. Keluar\n");
         printf("Pilihan >> ");
         scanf("%d", &pilihan);
 
@@ -130,11 +218,17 @@ void menuUtama() {
             pinjamBuku();
             break;
         case 3:
+            tampilListPeminjam();
+            break;
+        case 4:
+            //tambahPeminjam();
+            break;
+        case 5:
             break;
         default:
-            printf("Pilihan hanya dari 1 - 3 saja!\n");
+            printf("Pilihan hanya dari 1 - 5 saja!\n");
         }
-    } while(pilihan != 3);
+    } while(pilihan != 5);
 }
 
 void login() {
